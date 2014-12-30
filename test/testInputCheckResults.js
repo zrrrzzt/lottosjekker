49dc3719d45a0df1f3963abfe196b00c20e0c70e
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-12-30 21:12:42 +0100

    Initial commit

--- test/testInputCheckResults.js
+++ test/testInputCheckResults.js
@@ -3,3 +3,51 @@
 var assert = require('assert')
   , chkr = require('../lib/checkResults')
   ;
+
+describe('lottosjekker - inputs', function(){
+
+  it('requires opts.result to exist', function(done){
+
+    var opts = {
+          result: false,
+          rows: true
+        };
+
+    chkr(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: opts.result/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+  it('requires opts.rows to exist', function(done){
+
+    var opts = {
+          result:true,
+          rows:false
+        };
+
+    chkr(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: opts.rows/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
+});
--- test/testOutputCheckResults.js
+++ test/testOutputCheckResults.js
@@ -3,6 +3,24 @@
 var fs = require('fs')
   , assert = require('assert')
   , chkr = require('../lib/checkResults')
-  , results = JSON.parse(fs.readFileSync('results.json').toString())
-  , output = fs.readFileSync('output.txt').toString()
+  , results = JSON.parse(fs.readFileSync('test/results.json').toString())
+  , myrows = require('../myrows')
+  , output = fs.readFileSync('test/output.txt').toString()
   ;
+
+describe('lottosjekker - output', function(){
+
+  it('has expected output', function(done){
+
+    var opts = {
+      result: results,
+      rows: myrows
+    };
+
+    chkr(opts, function(err, data){
+      assert.equal(data, output);
+      done();
+    });
+  });
+
+});
